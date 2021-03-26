7b99d6ac68be76cb022988d24115025ef7add2be
Rafael Albano <74366109+Rafaelalbano@users.noreply.github.com>
2021-03-26 13:31:01 -0300

    Primeiros comandos com javascript.

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var nome = "Rafael Albano";
+var idade = 27;
+var frase = "Japão é o melhor time do mundo";
+alert(nome + " Tem " + idade + " anos ");
+console.log("teste de console")
+console.log(frase.replace("Japão", "Brasil"));
